8011bf129b97906bec396f69f9943cb01ae780b8
nacho452634 <rodrigueznacho897@gmail.com>
2023-04-21 09:50:00 +0200

    he practicat els parametre

--- cursBash/parametres.sh
+++ cursBash/parametres.sh
@@ -3,3 +3,17 @@
 NOM_SCRIPT=${0}
 
 echo "el parametre 0 indica el nom del script ${NOM_SCRIPT}"
+
+#veurem com funcionen els paràmetres 
+PARAMETRE=${0}
+#${0} ens indica el nom de l'executable
+echo "el parametre 0 és ${0}"
+
+#${1} és el primer parametre posicional
+echo "el parametre 1 es ${1}"
+
+#podeu escriure fins a 9 (verificar)
+echo "el parametre 2 és ${2}"
+
+parametre_sostingut=${#}
+echo "el parametre sostingut ${parametre_sostingut} indica el número de paràmetres
