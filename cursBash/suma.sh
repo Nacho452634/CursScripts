ac83d2b21cbd6678c053caede23a4e6c4d522e06
nacho452634 <rodrigueznacho897@gmail.com>
2023-04-24 09:36:49 +0200

    suma

--- cursBash/suma.sh
+++ cursBash/suma.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-echo "has introduit els numeros "
-SUMA=$((${1}+${2}))
-echo "${1}+${2}=${SUMA}"
+#control d'errades
+#si no ens passen dos paràmetres donam missatge d'errada i sortim del script
+NUM_PARAM=${#}
+if [[ ${NUM_PARAM} -ne 2 ]]
+then
+    echo "ERROR: has d'introduir dos paràmetres"
+    exit 1
+fi
+
+#aquest script ha de recollir dos paràmetres, els ha
+#de sumar i ha d'escriure per pantalla "a+b=resultat"
+
+
+A=${1}
+B=${2}
+SUMA=$((A+B))
+echo "${A}+${B}=${SUMA}"
