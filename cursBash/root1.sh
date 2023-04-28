e6b65d27ea4a1f48c82f144700384ba66bc7d538
nacho452634 <rodrigueznacho897@gmail.com>
2023-04-28 08:46:44 +0200

    usuari només per root

--- cursBash/root1.sh
+++ cursBash/root1.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+function usage(){
+    echo "Usage ${0}"
+    echo
+    echo "has de ser root"
+    echo "l'usuari no es pot repetir"
+    exit 1
+}
 
 #Scripts per crear un usuari
 
 #Primer comprovam si som l'usuari root
 if [[ ${UID} -ne 0 ]]
 then
-    echo "Usage ${0}"
-    echo
-    echo "has de ser root"
-    exit 1
+    usage
 fi
 
 read -p "Introdueix el nom d'usuari: " USER_NAME
