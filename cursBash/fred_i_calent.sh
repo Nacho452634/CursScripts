77d06bc9d0708479fb3ccb15e77c4cb2ad6d39fd
Nacho <irodriguezbo@esliceu.net>
2023-05-19 08:59:05 +0200

    fred i calent

--- cursBash/fred_i_calent.sh
+++ cursBash/fred_i_calent.sh
@@ -2,15 +2,28 @@
 
 #Genera un nombre aleatori entre 1 i 10:
 adivina=$(( ( RANDOM % 10 )  + 1 ))
+echo "adivina: ${adivina}"
 
 ########################################################
 # Escriu el teu codi aqui
-########################################################
+read -p "indica el teu numero: " A
 
+if [[ ${A} -eq ${adivina} ]]
+then
+    echo "Eureka!"
+    exit 0
+fi
 
+if [[ ${A} < ${adivina} ]]
+then
+    echo "Fred!"
+    exit 1
+fi
 
-########################################################
-# Fi del teu codi
-########################################################
+if [[ ${A} > ${adivina} ]]
+then
+    echo "Calent!"
+    exit 1
+fi
 
 echo "El nombre que havies d'adivinar era: ${adivina}"
